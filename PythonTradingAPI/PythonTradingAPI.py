4d8e46a317e44bfcaad38aa6f9c76f552c728a33
Lawson Shepherd <greatmindsinside@yahoo.com>
2021-01-30 15:23:48 -0700

    EnviromentSetup

--- PythonTradingAPI/PythonTradingAPI.py
+++ PythonTradingAPI/PythonTradingAPI.py
@@ -1,1 +1,11 @@
 
+import alpaca_backtrader_api as Alpaca
+import backtrader as bt
+import pytz
+from datetime import datetime
+from local_settings import alpaca_paper
+
+ALPACA_KEY_ID = alpaca_paper['api_key']
+ALPACA_SECRET_KEY = alpaca_paper['api_secret']
+ALPACA_PAPER = True
+
